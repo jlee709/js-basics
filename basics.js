c518b1431301712d4bcdecad7e4adaf287cba391
Justin Wayne Lee <justinlee709@amsesm.cnm7103.co.kr>
2017-08-14 15:48:18 +0900

    added canDrive var

--- basics.js
+++ basics.js
@@ -16,7 +16,9 @@ var person = {
  * if it should be true if your person object is at least 16 years old
  */
 
-
+if (person.age >= 16){
+	var canDrive = true;
+}
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
