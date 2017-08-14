f60383de87fc2f2dfaa7be52e0135356ebe36b53
Justin Wayne Lee <justinlee709@amsesm.cnm7103.co.kr>
2017-08-14 15:03:05 +0900

    added person object

--- basics.js
+++ basics.js
@@ -1,13 +1,16 @@
 /* Create a `myName` variable and assign it a String value */
 
-var myName = "Justin";
+var myName = "Justin;";
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
-
+var person = {
+	name: "Justin",
+	age: 28
+};
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
